f5ce2f9b0cc7b41bfff63334beea9e9c003da8f5
Oscar Ramirez <ing.oarrs@gmail.com>
2021-03-26 17:07:34 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { SwUpdate } from '@angular/service-worker';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  public updateAvailable: boolean = false;
+  constructor(
+    private updates: SwUpdate,
+  ) { }
+
+  ngOnInit() {
+
+
+  }
+
+  initializeApp() {
+    this.updates.available.subscribe((event) => {
+      this.updateAvailable = true;
+    });
+
+  }
 }
